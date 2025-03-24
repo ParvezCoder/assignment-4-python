9edb025d0ea2eebc1290c230ff9767965c64ddc0
Parvez Coder <ParvezCoder786@gmail.com>
2025-03-25 02:51:10 +0500

    deploye

---
+++ Assignment 00 to 05/00_intro_python/add_two_number.py
@@ -0,0 +1,10 @@
+def add_two_number():
+    num1 = input("Enter the First Number: ")
+    num1 = int(num1)
+    num2 = input("Enter the Second number: ")
+    num2 = int(num2)
+    sum  = num1 + num2
+
+    print("The sum of both number is = ", sum)
+if __name__ == "__main__":
+    add_two_number()
